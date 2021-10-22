900333276c2aea4ea2976aae95b6307fb9a54295
Carol Hu <jiecarolhu@gmail.com>
2021-10-22 14:46:29 -0700

    Add MinHeap and MaxHeap

---
+++ ClassLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("737fa376-5e99-4f1b-8acc-bfce28f7032f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ClassLibrary/MinHeap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class MinHeap<T> : MinMaxHeap<T> where T : IComparable<T>
+    {
+        public MinHeap(int size) :
+             base(size)
+        {
+        }
+
+        protected override void ReCalculateDown()
+        {
+            int index = 0;
+            while (HasLeftChild(index))
+            {
+                var smallerIndex = GetLeftChildIndex(index);
+                if (HasRightChild(index)
+                    && GetRightChild(index).CompareTo(GetLeftChild(index)) < 0)
+                //&& GetRightChild(index) < GetLeftChild(index))
+                {
+                    smallerIndex = GetRightChildIndex(index);
+                }
+
+                if (_elements[smallerIndex].CompareTo(_elements[index]) >= 0)
+                //  if (_elements[smallerIndex] >= _elements[index])
+                {
+                    break;
+                }
+
+                Swap(smallerIndex, index);
+                index = smallerIndex;
+            }
+        }
+
+        protected override void ReCalculateUp()
+        {
+            var index = _size - 1;
+            while (!IsRoot(index)
+                && _elements[index].CompareTo(GetParent(index)) < 0)
+            // && _elements[index] < GetParent(index))
+            {
+                var parentIndex = GetParentIndex(index);
+                Swap(parentIndex, index);
+                index = parentIndex;
+            }
+        }
+    }
+}
